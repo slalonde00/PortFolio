a1b54928a5c4a8f006f345d25f193496e2cbf29a
slalonde00 <slalonde00@gmail.com>
2021-08-27 11:06:47 -0400

    installation of bootstrap and app template

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from "axios";
 
-function App() {
-  return (
+
+const App2 = () => {
+    const [value, setValue] = useState();
+    const [repos, setRepos] = useState();
+  
+    const handleClick = async () => {
+      // fetch
+      const fetched = await axios.get(`https://api.github.com/users/${value}/repos`);
+      setRepos(fetched.data);
+      console.log(fetched);
+
+    }
+
+    
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  }
+  
+    const handleInputChange = (e) => {
+      setValue(e.target.value);
+    }
+ 
+    return (
+        
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+    <header className="App-header">
+      <form onSubmit={handleSubmit}>
+      <input type="text" value={value} onChange={handleInputChange} /><br></br>
+        <button variant="primary" onClick={handleClick} className="mt-3 mb-3">Submit</button>
+      </form>
+    </header>
+    <main className="App-main">
+    <Jumbotron>  
+    {repos   && repos.map(r => <div key={r.id}><label>{r.name} : </label><bouton variant="primary" className="mt-3 mb-3"><a href={r.html_url}>{r.html_url}</a></bouton>   </div>)}
+    </Jumbotron>    
+    </main>
+</div>      
+    )
+  }
+  export default App2;
